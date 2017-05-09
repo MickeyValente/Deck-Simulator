4757eda6c0ef84179a7c2d4a2eb33d5d5ad90eca
Miguel Angel Valente <miguel.valente@gonet.us>
2017-05-09 17:29:33 -0500

    Base del proyecto

---
+++ src/main/java/mx/com/ygo/deck/Card.java
@@ -0,0 +1,79 @@
+package mx.com.ygo.deck;
+
+import java.io.Serializable;
+
+/**
+ * @author Miguel Valente
+ *
+ */
+public class Card implements Serializable {
+	
+	private static final long serialVersionUID = 3050905619431691498L;
+	
+	private String description;
+	private String id;
+	private String name;
+	private String serial;
+	private String type;
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the serial
+	 */
+	public String getSerial() {
+		return serial;
+	}
+	/**
+	 * @param serial the serial to set
+	 */
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
